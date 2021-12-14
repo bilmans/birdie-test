455bf3e4912de9568750174a2b7a7899aabca60f
bilal.manssouri <bilal.manssouri@gmail.com>
2021-12-14 22:13:25 +0100

    modified nest serving static files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
-  if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
-    //@ts-ignore
-    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
-  }
 }
 bootstrap();
