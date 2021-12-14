77364a59b9eb750df55fa8788227e25d4afc4095
bilal.manssouri <bilal.manssouri@gmail.com>
2021-12-14 21:38:55 +0100

    added Procfile

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import express from 'express';
+import path from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
+  if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    //@ts-ignore
+    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+  }
 }
 bootstrap();
