e4a79de9354fbe3d2fbc93ae300f83f1a4c66456
bilal.manssouri <bilal.manssouri@gmail.com>
2021-12-15 15:56:41 +0100

    fixed build

--- dist/frontend/src/App.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import './App.css';
-import './css/table.css';
-declare function App(): JSX.Element;
-export default App;
--- dist/frontend/src/search.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import './css/search.css';
-declare const Search: ({ searchQuery, setSearchQuery }: any) => JSX.Element;
-export default Search;
